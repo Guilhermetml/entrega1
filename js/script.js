2816e818544b0ed285c0bb4a3c0276cbf481e902
Guilherme <guilhermeleite@byronsolutions.com>
2022-02-04 16:44:36 -0300

    pedindo socorro para Gimenez

---
+++ js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slides="principal"
+    time: 5000 // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    time: 5000,
+    nav: true
+  });
+}
